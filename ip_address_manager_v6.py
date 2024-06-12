7922f8e90b22ac24ef44ffb393ac69e89597bae9
Kavin Eksith <143245901+kavineksith@users.noreply.github.com>
2024-06-12 23:52:20 +0530

    Update ip_address_manager_v6.py
    Signed-off-by: Kavin Eksith <143245901+kavineksith@users.noreply.github.com>

--- ip_address_manager_v6.py
+++ ip_address_manager_v6.py
@@ -1,4 +1,8 @@
+import csv
 import ipaddress
+import json
+import os
+from pathlib import Path
 import sys
 
 
@@ -6,11 +10,11 @@ class IPAddressConverter:
     def __init__(self, ip):
         self.ip = ip
 
-    def to_decimal_and_hex(self):
+    def to_hex(self):
         try:
-            decimal_ip = int(ipaddress.ip_address(self.ip))
+            decimal_ip = int(ipaddress.IPv6Address(self.ip))
             hex_ip = hex(decimal_ip)
-            return decimal_ip, hex_ip
+            return hex_ip
         except ValueError:
             raise ValueError("Invalid IP address format")
 
@@ -160,6 +164,206 @@ def chunkstring(string, length, delimiter=':'):
         chunks = [string[i:i + length] for i in range(0, len(string), length)]
         return '.'.join(chunks)
 
+def result_to_csv(labels, data, save_path):
+    try:
+        # Ensure both labels and data have the same length
+        if len(labels) != len(data):
+            raise ValueError("Lengths of labels and data do not match.")
+        
+        # Specify the filename where you want to save the JSON data
+        filename = Path(save_path)
+
+        if not os.path.exists(filename):
+            # Open the file in write mode and immediately close it
+            with open(filename, 'w'):
+                pass
+
+        # Check if the first line exists
+        with open(filename, 'r') as check_file:
+            first_line = check_file.readline().strip()
+            # Open the file in append mode
+            with open(filename, 'a', newline='') as csvfile:
+                csvwriter = csv.writer(csvfile)
+                    
+                # If the first line doesn't exist, write the header
+                if not first_line:
+                    csvwriter.writerow(labels)
+
+                # Write data row
+                csvwriter.writerow([str(item) for item in data])
+    except ValueError as ve:
+        print(ve)
+    except FileNotFoundError:
+        print("Source file not found.")
+        sys.exit(1)
+    except PermissionError:
+        print("Permission denied to access the source or export file.")
+        sys.exit(1)
+    except KeyboardInterrupt:
+        print("Process interrupted by the user.")
+        sys.exit(1)
+    except Exception as e:
+        print("Error uploading data:", e)
+        sys.exit(1)
+
+def result_to_json(labels, data, save_path):
+    try:
+        # Ensure both labels and data have the same length
+        if len(labels) != len(data):
+            raise ValueError("Lengths of labels and data do not match.")
+        
+        # Create a dictionary pairing labels with data
+        json_data = {label: value for label, value in zip(labels, data)}
+
+        # Convert the dictionary to JSON string
+        json_output = json.dumps(json_data, indent=4)
+
+        # Specify the filename where you want to save the JSON data
+        filename = Path(save_path)
+
+        if not os.path.exists(filename):
+            # Open the file in write mode and immediately close it
+            with open(filename, 'w'):
+                pass
+
+        # Open the file in write mode and save the JSON data
+        with open(filename, "a") as json_file:
+            # json.dump(json_data, json_file, indent=4)
+            json_file.write(json_output)
+
+        print("JSON data has been saved to", filename)
+    except ValueError as ve:
+        print(ve)
+    except FileNotFoundError:
+        print("Source file not found.")
+        sys.exit(1)
+    except PermissionError:
+        print("Permission denied to access the source or export file.")
+        sys.exit(1)
+    except KeyboardInterrupt:
+        print("Process interrupted by the user.")
+        sys.exit(1)
+    except Exception as e:
+        print("Error uploading data:", e)
+        sys.exit(1)
+
+def result_to_display(labels, data):
+    try:
+        # Ensure both labels and data have the same length
+        if len(labels) != len(data):
+            raise ValueError("Lengths of labels and data do not match.")
+        
+        # Loop through each label and its corresponding data value
+        for label, value in zip(labels, data):
+            print(f"{label}: {value}")
+    except ValueError as ve:
+        print(ve)
+    except KeyboardInterrupt:
+        print("\nProcess interrupted by the user.")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def data_process(usr_ip_address, output_selection=1, save_path=None):
+    try:
+        given_ip_address, given_cidr = usr_ip_address.strip().split('/')
+        subnet_calculator = SubnetCalculator(given_ip_address, int(given_cidr))
+
+        ip_type = subnet_calculator.ip_type()
+        network_address = subnet_calculator.network_address()
+        broadcast_address = subnet_calculator.broadcast_address()
+        total_hosts = subnet_calculator.total_number_of_hosts()
+        usable_hosts = subnet_calculator.number_of_usable_hosts()
+        cidr_notation = subnet_calculator.cidr_notation()
+        usable_host_range_start, usable_host_range_end = subnet_calculator.usable_host_ip_range()
+        usable_host_range_str = f"{usable_host_range_start} - {usable_host_range_end}" if usable_host_range_start and usable_host_range_end else "N/A"
+
+        ip_converter = IPAddressConverter(given_ip_address)
+        binary_ip = ip_converter.to_binary()
+        decimal_ip = ip_converter.to_decimal()
+        hex_ip_raw = ip_converter.to_hex()
+        hex_ip = ipaddress.IPv6Address(hex_ip_raw).exploded
+
+        subnet_calculator = SubnetCalculator(given_ip_address, int(given_cidr))
+        subnet, subnet_mask = subnet_calculator.calculate_subnet()
+        subnet_mask_bin = subnet_calculator.subnet_mask_binary()
+        subnet_bin = subnet_calculator.subnet_binary()
+        host_mask = subnet_calculator.host_mask_calculator()
+        host_mask_bin = subnet_calculator.host_mask_binary()
+
+        # Convert subnet, subnet mask, and host mask to hexadecimal
+        subnet_hex = subnet.exploded
+        subnet_mask_hex = subnet_mask.exploded
+        host_mask_hex = ipaddress.IPv6Address(int(host_mask_bin, 2)).exploded
+
+        labels = [
+            "IPv6 address",
+            "IPv6 Type",
+            "Network Address",
+            "Broadcast Address",
+            "Total Number of Hosts",
+            "Number of Usable Hosts",
+            "CIDR Notation",
+            "Usable Host IP Range",
+            "Decimal representation",
+            "Hexadecimal representation",
+            "Binary representation",
+            "Subnet",
+            "Subnet mask",
+            "Host mask",
+            "Subnet binary",
+            "Subnet mask binary",
+            "Host mask binary",
+            "Subnet hexadecimal representation",
+            "Subnet mask hexadecimal representation",
+            "Host mask hexadecimal representation",
+            "Subnet decimal representation",
+            "Subnet mask decimal representation",
+            "Host mask decimal representation"
+        ]
+
+        data = [
+            str(given_ip_address),
+            str(ip_type),
+            str(network_address),
+            str(broadcast_address),
+            str(total_hosts),
+            str(usable_hosts),
+            f'/{cidr_notation}',
+            str(usable_host_range_str),
+            str(decimal_ip),
+            str(hex_ip),
+            str(binary_ip),
+            f'{subnet}/{given_cidr}',
+            str(subnet_mask),
+            str(host_mask),
+            str(chunkstring(subnet_bin, 8)),
+            str(chunkstring(subnet_mask_bin, 8)),
+            str(chunkstring(host_mask_bin, 8)),
+            str(subnet_hex),
+            str(subnet_mask_hex),
+            str(host_mask_hex),
+            str(int(subnet)),
+            str(int(subnet_mask)),
+            str(int(host_mask_bin, 2))
+        ]
+
+        if output_selection == 1:
+            result_to_display(labels, data)
+        elif output_selection == 2:
+            result_to_csv(labels, data, save_path)
+        elif output_selection == 3:
+            result_to_json(labels, data, save_path)
+        # else:
+        #     print("Invalid selection. Please enter a number between 1 and 3.")
+
+    except ValueError as ve:
+        print(ve)
+    except KeyboardInterrupt:
+        print("\nProcess interrupted by the user.")
+        sys.exit(1)
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def main():
     while True:
@@ -168,68 +372,40 @@ def main():
             if usr_ip_address.lower() == 'exit':
                 print("Exiting the program.")
                 sys.exit(0)
-            
-            given_ip_address, given_cidr = usr_ip_address.strip().split('/')
-            subnet_calculator = SubnetCalculator(given_ip_address, int(given_cidr))
-
-            ip_type = subnet_calculator.ip_type()
-            network_address = subnet_calculator.network_address()
-            broadcast_address = subnet_calculator.broadcast_address()
-            total_hosts = subnet_calculator.total_number_of_hosts()
-            usable_hosts = subnet_calculator.number_of_usable_hosts()
-            cidr_notation = subnet_calculator.cidr_notation()
-            usable_host_range_start, usable_host_range_end = subnet_calculator.usable_host_ip_range()
-            usable_host_range_str = f"{usable_host_range_start} - {usable_host_range_end}" if usable_host_range_start and usable_host_range_end else "N/A"
-
-            ip_converter = IPAddressConverter(given_ip_address)
-            binary_ip = ip_converter.to_binary()
-            decimal_ip = ip_converter.to_decimal()
-            print(f"IPv6 address: {given_ip_address}")
-            print(f"IP Type: {ip_type}")
-            print(f"Network Address: {network_address}")
-            print(f"Broadcast Address: {broadcast_address}")
-            print(f"Total Number of Hosts: {total_hosts}")
-            print(f"Number of Usable Hosts: {usable_hosts}")
-            print(f"CIDR Notation: /{cidr_notation}")
-            print(f"Usable Host IP Range: {usable_host_range_str}")
-            print(f"IP address hexadecimal representation: {binary_ip}")
-            print(f"IP address decimal representation: {decimal_ip}")
-
-            # subnet_calculator = SubnetCalculator(given_ip_address, int(given_cidr))
-            subnet, subnet_mask = subnet_calculator.calculate_subnet()
-            subnet_mask_bin = subnet_calculator.subnet_mask_binary()
-            subnet_bin = subnet_calculator.subnet_binary()
-            host_mask = subnet_calculator.host_mask_calculator()
-            host_mask_bin = subnet_calculator.host_mask_binary()
-
-            # Convert subnet, subnet mask, and host mask to hexadecimal
-            subnet_hex = subnet.exploded
-            subnet_mask_hex = subnet_mask.exploded
-            host_mask_hex = ipaddress.IPv6Address(int(host_mask_bin, 2)).exploded
-
-            print(f"Subnet: {subnet}/{given_cidr}")
-            print(f"Subnet mask: {subnet_mask}")
-            print(f"Host mask: {host_mask}\n")
-
-            print(f"Subnet binary: {chunkstring(subnet_bin, 8)}")
-            print(f"Subnet mask binary: {chunkstring(subnet_mask_bin, 8)}")
-            print(f"Host mask binary: {chunkstring(host_mask_bin, 8)}\n")
-
-            print(f"Subnet hexadecimal representation: {subnet_hex}")
-            print(f"Subnet mask hexadecimal representation: {subnet_mask_hex}")
-            print(f"Host mask hexadecimal representation: {host_mask_hex}\n")
-
-            print(f"Subnet decimal representation: {int(subnet)}")
-            print(f"Subnet mask decimal representation: {int(subnet_mask)}")
-            print(f"Host mask decimal representation: {int(host_mask_bin, 2)}\n")
-        except ValueError as ve:
-            print(ve)
+            elif usr_ip_address.lower() == "multiple":
+                file_location = Path(input("Enter the file location: "))
+                if not os.path.exists(file_location):
+                    raise FileNotFoundError
+                else:
+                    try:
+                        output_selection = int(input("Select a method (2 for CSV, 3 for JSON): "))
+                        if output_selection in [2, 3]:
+                            save_path = Path(input("Enter the file location: "))
+                        else:
+                            print("Invalid selection. Please enter a number between 2 and 3.")
+                            sys.exit(1)
+                        with open(file_location, 'r', encoding='utf-8') as ip_list:
+                            for item in ip_list:
+                                data_process(item, output_selection, save_path)
+                    except PermissionError:
+                        print("Permission denied to access the source or export file.")
+                        sys.exit(1)
+            else:
+                data_process(usr_ip_address, output_selection=1, save_path=None)
+        except ValueError:
+            print("Invalid input. Please enter a valid IPv6 address followed by CIDR notation (e.g., 2001:0db8:85a3:0000:0000:8a2e:0370:7334/64).")
+        except FileNotFoundError:
+            print("Source file not found.")
+            sys.exit(1)
+        except PermissionError:
+            print("Permission denied to access the source or export file.")
+            sys.exit(1)
         except KeyboardInterrupt:
             print("\nProcess interrupted by the user.")
             sys.exit(1)
         except Exception as e:
             print(f"An error occurred: {e}")
-
+            
 
 if __name__ == "__main__":
     main()
